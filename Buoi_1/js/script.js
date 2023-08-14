dbd3aa221db757bb5dc06b2e1dd0aa3ba357abd9
LeoDomGit <leodomsolar@gmail.com>
2023-08-14 16:16:18 +0700

    Commit Lab1

--- Buoi_1/js/script.js
+++ Buoi_1/js/script.js
@@ -1,12 +1,27 @@
 // Function : hàm 
 // function Ten_Ham
-var name = '';
 
+var arr=[];
 function function1(){
     // Let : Khai báo biến cục bộ 
     // Biến cục bộ thì sẽ 
     //ko thể sử dụng ngoài function 
     // Var tenBien  : Khai báo biến toàn cục 
+    var name = '';
     name= document.getElementById('fullname').value;
-    console.log(name);
+
+    arr.push(name);
+    const div = document.getElementsByClassName('div1');
+    console.log(arr);
+    // array.length
+
+    var str=``;
+
+    arr.forEach(el => {
+        str+=`<br>`+el+`<br> `
+    });
+    console.log(str);
+    // Render HTML Tag
+    div[1].innerHTML=str; 
+
 }
