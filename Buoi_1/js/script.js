f0cc4baee34c0415e650a8bda3cdd1a1396d41fe
LeoDomGit <leodomsolar@gmail.com>
2023-08-14 15:33:22 +0700

    C

---
+++ Buoi_1/js/script.js
@@ -0,0 +1,12 @@
+// Function : hàm 
+// function Ten_Ham
+var name = '';
+
+function function1(){
+    // Let : Khai báo biến cục bộ 
+    // Biến cục bộ thì sẽ 
+    //ko thể sử dụng ngoài function 
+    // Var tenBien  : Khai báo biến toàn cục 
+    name= document.getElementById('fullname').value;
+    console.log(name);
+}
