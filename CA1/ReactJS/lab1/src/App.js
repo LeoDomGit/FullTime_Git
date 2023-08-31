ffce798862abeb714c3cdf1926b36c30347274b6
LeoDomGit <leodomsolar@gmail.com>
2023-08-31 09:28:21 +0700

    C

---
+++ CA1/ReactJS/lab1/src/App.js
@@ -0,0 +1,11 @@
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+function App() {
+  return (
+    <div className="App">
+      <Products/>
+    </div>
+  );
+}
+
+export default App;
